1490bbe3876e16e2edb2faff418e07799882dde3
Dixon-obabo <cheche.oloo@gmail.com>
2021-03-01 22:34:39 +0300

    fixing the sign in page

--- app/src/main/java/com/example/chamka/Sign_in.java
+++ app/src/main/java/com/example/chamka/Sign_in.java
@@ -2,24 +2,39 @@ package com.example.chamka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Sign_in extends AppCompatActivity {
 
     EditText mail;
     EditText password;
+    FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         mail=findViewById(R.id.email);
         password=findViewById(R.id.password);
+        auth=FirebaseAuth.getInstance();
     }
 
     public void signin(View view) {
         Toast.makeText(this, mail.getText().toString(), Toast.LENGTH_SHORT).show();
+        auth.signInWithEmailAndPassword(mail.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                Intent intent= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
